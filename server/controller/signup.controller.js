2325c5cd285de0fe4dd79768d42acb77d8cec9ef
abrahamjo25 <abirham.johannes@gmail.com>
2024-06-06 16:28:05 +0300

    create signin api route

--- server/controller/signup.controller.js
+++ server/controller/signup.controller.js
@@ -1,6 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+
+import jwt from "jsonwebtoken";
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
@@ -17,6 +19,39 @@ export const signup = async (req, res, next) => {
       res.status(201).json({ message: "User created successfully" });
     } catch (error) {
       next(error);
+    }
+  } else {
+    next(errorHandler(400, "Fill all required fields"));
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (email && password) {
+    try {
+      const user = await User.findOne({ email });
+
+      if (user) {
+        if (bcryptjs.compareSync(password, user.password)) {
+          const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+            expiresIn: "1h",
+          });
+          const { password: pass, ...restInfo } = user._doc;
+
+          res
+            .status(200)
+            .cookie("accessToken", token, {
+              httpOnly: true,
+            })
+            .json(restInfo);
+        } else {
+          next(errorHandler(400, "Invalid password"));
+        }
+      } else {
+        next(errorHandler(400, "Invalid username"));
+      }
+    } catch (err) {
+      next(err);
     }
   } else {
     next(errorHandler(400, "Fill all required fields"));
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { signup } from "../controller/signup.controller.js";
+import { signin, signup } from "../controller/signup.controller.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 
-// router.get("/signup", allUsers);
+router.post("/signin", signin);
 
 export default router;
